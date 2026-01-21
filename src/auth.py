773eb84efe6baee512e907a71e3affd84daee8e5
hoyong.shin <hoyong6737@gmail.com>
2026-01-21 15:50:53 +0900

    auth.py파일 작성

--- src/auth.py
+++ src/auth.py
@@ -1,6 +1,7 @@
 import os
 import requests
 import json
+import time
 from dotenv import load_dotenv
 
 
@@ -27,3 +28,17 @@ class KISAuth:
             "appkey": self.app_key,
             "appsecret": self.app_secret
         }
+
+        response = requests.post(f"{self.url}{path}", headers=headers, data=json.dumps(body))
+
+        if response.status_code == 200:
+            return response.json().get("access_token")
+        else:
+            print(f"Error: {response.json()}")
+            return None
+        
+if __name__ == "__main__":
+    # 테스트 실행
+    auth = KISAuth(mode="mock")
+    token = auth.get_access_token()
+    print(f"발급된 토큰: {token}")
